394e537d037fbc8bc08158afb99ef185aeffd6c0
Itayzrihan <Itayyzrihan@gmail.com>
2018-01-13 17:15:51 +0200

    Money Joker

---
+++ build/release/external/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/itay/Desktop/Joker/external/gtest
+# Build directory: /home/itay/Desktop/Joker/build/release/external/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
